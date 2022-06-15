3c76c1355edf2e997ea6e45a0f1c1140c1d65f1d
Aathish-R <aathirajamanipgv@gmail.com>
2022-06-15 13:17:15 +0530

    Add files via upload
    Files uploaded

---
+++ main.js
@@ -0,0 +1,15 @@
+$('.fade-slider-parent').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    slidesToScroll: 1,
+    arrows: false,
+    speed: 300
+});
+
+$('.testimonial-parent').slick({
+    slidesToShow: 2,
+    autoplay: true,
+    slidesToScroll: 1,
+    arrows: true,
+    speed: 300
+});
